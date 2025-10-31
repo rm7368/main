df043e08ad70f769ba0ef2b73a1e70cc7c49bacf
rm7368 <Razvan.Matei@nyulangone.org>
2025-10-31 10:14:37 -0400

    Python autoencoder script

--- hpc-scripts/zwick2024scripts/src/dataset.py
+++ hpc-scripts/zwick2024scripts/src/dataset.py
@@ -8,24 +8,24 @@ class EnterocyteDataset(Dataset):
   def __init__(self, expr_file, labels_file):
     """
     Args:
-      expr_file: Path to CSV with expression (cellxgene)
-      labels_file: Path to CSV with domain labels
+    expr_file: Path to CSV with expression (cellxgene)
+    labels_file: Path to CSV with domain labels
     """
     self.expression = pd.read_csv(expr_file).values.astype(np.float32)
     labels_df = pd.read_csv(labels_file)
-    
+  
     domain_map = {
-      'Domain_A': 0,
-      'Domain_B': 1,
-      'Domain_C': 2,
-      'Domain_D': 3,
-      'Domain_E': 4
+    'Domain_A': 0,
+    'Domain_B': 1,
+    'Domain_C': 2,
+    'Domain_D': 3,
+    'Domain_E': 4
     }
     self.domain_labels = np.array([domain_map[d] for d in labels_df['domain']])
-    
+  
     self.n_cells, self.n_genes = self.expression.shape
     self.n_domains = 5
-    
+  
     print(f"Loaded {self.n_cells} cells x {self.n_genes} genes")
     print(f"Domain distribution: {np.bincount(self.domain_labels)}")
     
@@ -42,6 +42,7 @@ class EnterocyteDataset(Dataset):
       'domain': torch.LongTensor([self.domain_labels[idx]])[0]
     }
     
-  def get_dataloaders(train_expr, train_labels, test_expr, test_labels,
+  def get_dataloaders(train_expr, train_labels, test_expr, test_labels, 
                       batch_size=128, num_workers=4):
+    
       
